7fc4fe328506d6e6e81cb4ae01ae5a71d8f1a88e
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-01-24 07:19:29 +0000

    different applications of functions

--- hello.js
+++ hello.js
@@ -2,8 +2,17 @@ const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
 
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
 
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+sayHelloToConsole('John'); 
+const greeting = returnSayHello('John');
+console.log(greeting);
